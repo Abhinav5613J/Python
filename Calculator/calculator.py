82375dbd70464954e687028822d6478e8b6c167b
Abhinav5613J <97839623+Abhinav5613J@users.noreply.github.com>
2022-03-31 15:53:54 +0530

    Update calculator.py

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,6 +1,6 @@
 a=int(input("Enter the first number: "))
 b=int(input("Enter the second number: "))
-c=input("ENter the operators to perform the calculations('+','-','*','/'): ")
+c=input("Enter the operators to perform the calculations('+','-','*','/'): ")
 if(c=="+"):
   print("Your answer is: ",a+b)
 elif(c=="-"):
