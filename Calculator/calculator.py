8b9738c3b14d9d31937b5347ae076a6e5fb250b5
Abhinav5613J <97839623+Abhinav5613J@users.noreply.github.com>
2022-03-31 15:51:01 +0530

    Create calculator.py

---
+++ Calculator/calculator.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=input("ENter the operators to perform the calculations('+','-','*','/'): ")
+if(c=="+"):
+  print("Your answer is: ",a+b)
+elif(c=="-"):
+  print("Your answser is: ",a-b)
+elif(c=="*"):
+  print("Your answer is: ",a*b)
+elif(c=="/"):
+  print("Your answer is: ",a/b)
+else:
+  print("Please enter the valid operator!")
